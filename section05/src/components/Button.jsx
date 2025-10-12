d5b2a2425b6a4be0ecfc98095a1d234d14ee7bdd
kkhhjjoo <khjoo1203@gmail.com>
2025-10-12 20:42:10 +0900

    리액트 공부

---
+++ section05/src/components/Button.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+const Button = ({ children, text, color = "black" }) => {
+  const onClickButton = (e) => {
+    console.log(e);
+  console.log(text);
+}
+  return (
+    <button
+    onClick={onClickButton}
+    //onMouseEnter={onClickButton}
+    style={{ color: color }}>{text} - {color.toUpperCase()}
+    {children}
+  </button>
+  );
+};
+
+
+export default Button;
