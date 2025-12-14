7ba8eabc86f9fa5575f6ecdc00eae99a2149aa98
kkhhjjoo <khjoo1203@gmail.com>
2025-12-14 21:26:54 +0900

    useCallback과 함수 재생성 방지

--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useRef, useReducer } from 'react';
+import { useState, useRef, useReducer, useCallback } from 'react';
 import Editor from './components/Editor';
 import Header from './components/Header';
 import List from './components/List';
@@ -44,14 +44,26 @@ function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
-    // const newTodo = {
-    //   id: idRef.current++,
-    //   isDone: false,
-    //   content: content,
-    //   date: new Date().getTime(),
-    // };
-    // setTodos([newTodo, ...todos]);
+  // const onCreate = (content) => {
+  // const newTodo = {
+  //   id: idRef.current++,
+  //   isDone: false,
+  //   content: content,
+  //   date: new Date().getTime(),
+  // };
+  // setTodos([newTodo, ...todos]);
+  //   dispatch({
+  //     type: 'CREATE',
+  //     data: {
+  //       id: idRef.current++,
+  //       isDone: false,
+  //       content: content,
+  //       date: new Date().getTime(),
+  //     },
+  //   });
+  // };
+
+  const onCreate = useCallback((content) => {
     dispatch({
       type: 'CREATE',
       data: {
@@ -61,27 +73,32 @@ function App() {
         date: new Date().getTime(),
       },
     });
-  };
+  }, []);
 
-  const onUpdate = (targetId) => {
-    //todos State의 값들 중에
-    //targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
-    //인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
-    // setTodos(todos.map((todo) => (todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo)));
+  // const onUpdate = (targetId) => {
+  //todos State의 값들 중에
+  //targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
+  //인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
+  // setTodos(todos.map((todo) => (todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo)));
+  // dispatch({
+  //   type: 'UPDATE',
+  //   targetId: targetId,
+  // });
+  // };
+
+  const onUpdate = useCallback((targetId) => {
     dispatch({
       type: 'UPDATE',
       targetId: targetId,
     });
-  };
+  }, []);
 
-  const onDelete = (targetId) => {
-    //인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
-    // setTodos(todos.filter((todo) => todo.id !== targetId));
+  const onDelete = useCallback((targetId) => {
     dispatch({
       type: 'DELETE',
       targetId: targetId,
     });
-  };
+  }, []);
 
   return (
     <div className="App">
